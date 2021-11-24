dfede20daa3eca524fea341be475f7bf6f889a33
an1age <vladimirtyo7@gmail.com>
2021-11-24 14:36:15 +0600

    Добавление класса Geometry

---
+++ MSP/Geometry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSP
+{
+    class Geometry
+    {
+        public double GetSquareArea(double a)
+        {
+            return a * a;
+        }
+        public double GetTriangleArea(double a, double h)
+        {
+            return a * h / 2;
+        }
+        public double GetRectangleArea(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
